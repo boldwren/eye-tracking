7a41bc70e151a4da61ba279e257b47984a9864c4
David Wren <boldwren@gmail.com>
2020-01-25 23:48:19 +0000

    make category montages

--- expand-videos.sh
+++ expand-videos.sh
@@ -2,11 +2,8 @@
 
 set -euxo pipefail
 
-TMPDIR=$HOME/tmp
-SRCDIR=$HOME/Downloads/input/
-
-for f in $(cd $SRCDIR && find . -name '*.mp4' | sed 's:^[.]/::' )
-do
+function expand() {
+    f="${1}"
     echo $f
     resolution=$(
         ffprobe \
@@ -14,11 +11,16 @@ do
             -select_streams v:0 \
             -show_entries stream=width,height \
             -of csv=s=x:p=0 \
-            $SRCDIR/$f
+            $SRCDIR/$f |
+            tr ' ' '\n' |
+            head -n 1
     )
 
+    echo "RESOLUTION" $resolution
+
     imgdir=$TMPDIR/$resolution/$f.images
-    [[ ! -f $imgdir/done ]] &&
+    if [[ ! -f $imgdir/done ]]
+    then
         mkdir -p $imgdir &&
         ffmpeg \
             -nostdin \
@@ -30,9 +32,11 @@ do
             $imgdir/keyframe-%05d.jpeg &&
         ffprobe -v quiet -select_streams v -skip_frame nokey -show_frames -show_entries frame=pkt_pts_time $SRCDIR/$f > $imgdir/keyframes.list &&
         touch $imgdir/done
-    
+    fi
+
     hlsdir=$TMPDIR/$resolution/$f.hls
-    [[ ! -f $hlsdir/done ]] &&
+    if [[ ! -f $hlsdir/done ]]
+    then
         mkdir -p $hlsdir &&
         ffmpeg \
             -nostdin \
@@ -45,4 +49,23 @@ do
             -hls_segment_filename $hlsdir/segment-%05d.ts \
             $hlsdir/out.m3u8 &&
         touch $hlsdir/done
-done
+    fi
+}
+
+if [ $# -eq 0 ]
+then
+    SRCDIR=$HOME/Downloads/input/
+    TMPDIR=$HOME/tmp
+    for f in $(cd $SRCDIR && find . -name '*.mp4' | sed 's:^[.]/::' )
+    do
+        expand "${f}"
+    done
+elif [ $# -eq 3 ]
+then
+    SRCDIR="${1}"
+    TMPDIR="${2}"
+    f="${3}"
+    expand "${f}"
+else
+    echo "please provide either 0 arguments or SRCDIR TARGETDIR filename"
+fi
