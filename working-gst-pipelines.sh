2a70c96e5c59f2a934cf16f9dd0a9e7dbc397389
David Wren <boldwren@gmail.com>
2020-02-17 22:48:58 +0000

    use gstreamer to cut videos together

--- working-gst-pipelines.sh
+++ working-gst-pipelines.sh
@@ -1,20 +1,37 @@
+set -e
+
 # working pipelines
 export GST_DEBUG_DUMP_DOT_DIR=$HOME/tmp/gst-debug-files/
+echo
+echo
+echo
+echo
+echo
+echo
+(
+    head -n $((3 * 10)) \
+        $HOME/tmp/montage-interleaved-1920x1080-Cosplay.m3u8 &&
+    tail -n 1 \
+        $HOME/tmp/montage-interleaved-1920x1080-Cosplay.m3u8
+) |
+    tee $HOME/tmp/head.m3u8
 
+echo
+echo
 gst-launch-1.0 \
-    souphttpsrc location=http://localhost:5000/1920x1080/all-videos/ph5c0efef67c779.mp4.hls/filtered.m3u8 \
+    filesrc location=$HOME/tmp/montage-interleaved-1920x1080-Cosplay.m3u8 \
         ! hlsdemux ! tsdemux name=dem \
         ! multiqueue name=queue \
         ! h264parse \
-        ! matroskamux name=mux ! filesink location=out.mkv \
+        ! mpegtsmux name=mux ! filesink location=$HOME/tmp/montage-interleaved-1920x1080-Cosplay.ts \
     dem. ! queue. \
-    queue. ! aacparse ! avdec_aac ! audioconvert ! mux.
+    queue. ! aacparse ! mux. \
 
 # ^^^ OMG I DID IT.
-
+echo "DONE"
 
 true gst-launch-1.0 \
-    souphttpsrc location=http://localhost:5000/1920x1080/all-videos/ph5c0efef67c779.mp4.hls/filtered.m3u8 \
+    souphttpsrc location=http://localhost:5000/1920x1080/all-videos/ph5c4402553423d.mp4.hls/filtered.m3u8 \
         ! hlsdemux name=demux ! tsdemux name=dem \
         ! multiqueue name=queue \
         ! h264parse ! vtdec ! autovideosink \
